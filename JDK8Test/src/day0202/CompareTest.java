29dd3babb3e06c85ecf5e523520a197470150a16
mezier <mezier777@gmail.com>
2018-02-02 22:01:00 +0000

    update

--- JDK8Test/src/day0202/CompareTest.java
+++ JDK8Test/src/day0202/CompareTest.java
@@ -5,20 +5,20 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+interface Con<A,T>{
+	void convert(A from);
+}
 public class CompareTest {
-
 	public static <T> void main(String[] args) {
 		List<String> names = Arrays.asList("peter", "anna", "bob");
-		//method 1: annoymous class
-		/*
-		Collections.sort(names, new Comparator<String>() {
+		//method 1: annoymous class	
+		/*Collections.sort(names, new Comparator<String>() {
 			@Override
 			public int compare(String o1, String o2) {
 				// TODO Auto-generated method stub
 				return o2.compareTo(o1);
 				// descending o2.compareTo(o1);
 			}
-
 		});
 		System.out.println(names.toString());*/
 		Collections.sort(names,(String a, String b) -> {
