6d10dae93d36e1ec2c92ab873240c080ca3a8f0b
mezier <mezier777@gmail.com>
2018-02-02 21:31:47 +0000

    add

---
+++ JDK8Test/src/day0202/CompareTest.java
@@ -0,0 +1,30 @@
+package day0202;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class CompareTest {
+
+	public static <T> void main(String[] args) {
+		List<String> names = Arrays.asList("peter", "anna", "bob");
+		//method 1: annoymous class
+		/*
+		Collections.sort(names, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				// TODO Auto-generated method stub
+				return o2.compareTo(o1);
+				// descending o2.compareTo(o1);
+			}
+
+		});
+		System.out.println(names.toString());*/
+		Collections.sort(names,(String a, String b) -> {
+			 return b.compareTo(a);
+			
+		});
+		System.out.println(names.toString());
+	}
+}
