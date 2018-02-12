29c2768877d59258b1cc02883d1191011ad5d793
mezier <mezier777@gmail.com>
2018-02-12 15:29:15 +0000

    del

--- mavenPrac/src/main/java/dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import po.Student;
-
-import java.util.List;
-
-public interface StudentDao {
-    public List<Student> printAll();
-}
--- mavenPrac/target/maven/index.jsp
+++
@@ -1,9 +0,0 @@
-
-<html>
-<body>
-<h2>Hello World!</h2>
-Hello ! world.
-Hello , good morning
-<a href="stu" >list</a>
-</body>
-</html>
