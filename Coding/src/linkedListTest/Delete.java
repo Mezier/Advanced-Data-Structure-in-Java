f207b0c2849a345ec65eb7500975c0c7f04527e7
mezier <mezier777@gmail.com>
2018-02-26 19:49:42 +0000

    update

---
+++ Coding/src/linkedListTest/Delete.java
@@ -0,0 +1,40 @@
+package linkedListTest;
+
+import java.util.LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){val=x;}
+}
+public class Delete {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
