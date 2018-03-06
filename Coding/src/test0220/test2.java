29aa14e3dcbdf9a4aa49034d967c8c4a3744fe40
mezier <mezier777@gmail.com>
2018-03-06 23:47:25 +0000

    add

--- Coding/src/test0220/test2.java
+++ Coding/src/test0220/test2.java
@@ -3,9 +3,15 @@ package test0220;
 import java.util.Set;
 
 public class test2 {
+    static Integer a=new Integer(0);
+    public void sum(){
+        a+=1;
+    }
     public static void main(String[] args) {
-        Object o;
-
-        //if(o.toString().contains());
+test2 t=new test2();
+        for(int i=0;i<5;i++){
+            t.sum();
+        }
+        System.out.println(a);
     }
 }
