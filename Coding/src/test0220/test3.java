e0a201430b40c7eaf2418e4e65cda93f1c67a332
mezier <mezier777@gmail.com>
2018-03-08 23:56:13 +0000

    1

--- Coding/src/test0220/test3.java
+++ Coding/src/test0220/test3.java
@@ -2,10 +2,10 @@ package test0220;
 
 public class test3 {
     public static void main(String[] args) {
-        String s="VGDOOGZGZVMIDIBDNVYVIBMJPNOCDIBYMDIFYZZKJMOVNOZIJ";
+        String s="CHAINFUSION";   //OTMUZRGEUAZ
         StringBuffer sb=new StringBuffer();
         for(int i=0;i<s.length();i++){
-            char temp= (char) ((s.charAt(i)-65+15)%26+65);
+            char temp= (char) ((s.charAt(i)-65+12)%26+65);
             sb.append(temp);
         }
         System.out.println(sb.toString().toLowerCase());
