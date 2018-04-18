7e92e95d287562d8ec3194bd9d37c072b5e74094
mezier <mezier777@gmail.com>
2018-04-18 09:05:17 +0100

    add

--- Coding/src/informationSecurity/test.java
+++ Coding/src/informationSecurity/test.java
@@ -3,7 +3,7 @@ package informationSecurity;
 public class test {
     public static void main(String[] args) {
         //long l=Double.doubleToLongBits(Math.pow(4,937));
-        double l=Math.pow(4,7);
-        System.out.println(l%11);
+        double l=Math.pow(27,27);
+        System.out.println(l%55);
     }
 }
---
+++ SQL/src/main/java/utils/DBConnect.java
@@ -0,0 +1,84 @@
+package utils;
+
+import java.sql.*;
+
+public class DBConnect {
+    private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+
+    public Connection getConnect() {
+        return connect;
+    }
+
+    public void disconnect() {
+        try {
+            connect.close();
+        } catch (Exception e) {
+        }
+
+    }
+
+    public void connect(String username, String password) {
+        // pass your mysql username and password to connect
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/test" + "user=" + username + "&password=" + password + "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createDatabase(String name) {
+        //creating a database
+        try {
+            String sql = "create database if not exists " + name + ";";
+            if (this.connect != null) {
+                Statement s = this.connect.createStatement();
+                s.executeUpdate(sql);
+            } else {
+                System.out.println("No connection");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void useDatabase(String name) {
+        //setting the active database
+        try {
+            String sql = "use " + name + ";";
+            Statement s = this.connect.createStatement();
+            s.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void executingDDL(String sql) {
+        // executing DDL sql (create, alter)
+        try {
+            if (this.connect != null) {
+                Statement s = this.connect.createStatement();
+                s.executeUpdate(sql);
+            } else {
+                System.out.println("No connection");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void executingDML(String sql) {
+        // executing DML sql
+        try {
+            Statement st = this.connect.createStatement();
+            st.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
