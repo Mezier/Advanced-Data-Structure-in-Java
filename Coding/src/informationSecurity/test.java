12241591a8fd6d7b484557c689badaaa2519a4f2
mezier <mezier777@gmail.com>
2018-04-19 21:36:28 +0100

    update

--- Coding/src/informationSecurity/test.java
+++ Coding/src/informationSecurity/test.java
@@ -3,7 +3,7 @@ package informationSecurity;
 public class test {
     public static void main(String[] args) {
         //long l=Double.doubleToLongBits(Math.pow(4,937));
-        double l=Math.pow(27,27);
-        System.out.println(l%55);
+        double le=Math.pow(4,939);
+        System.out.println(le%11);
     }
 }
